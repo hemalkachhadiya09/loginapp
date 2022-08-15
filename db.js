953a60a52a8babb737e720d826215e43af4843a4
Hemalkachhadiya09 <kachhadiyahemal3@gmail.com>
2022-08-15 12:02:55 +0530

    code

---
+++ db.js
@@ -0,0 +1,2 @@
+let mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://admin:admin1234@cluster0.rursfne.mongodb.net/?retryWrites=true&w=majority/jiomartdb')
---
+++ model/userModel.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    phone:Number,
+    role:String
+})
+
+mongoose.model('user', userSchema);
+module.exports = mongoose.model('user')
